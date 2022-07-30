96bb585f8fb74009cccaecc80487e5e65a37a76d
Patryk Lezon <Plezo@protonmail.com>
2022-07-29 20:05:46 -0400

    Added time delay + line checker (Playable)

--- main.cpp
+++ main.cpp
@@ -13,6 +13,34 @@ std::vector<std::vector<char>> board(BOARD_HEIGHT, std::vector<char>(BOARD_WIDTH
 // player is current pos, shadow is your potential next pos
 std::pair<int, int> player[4], shadow[4];
 
+int figures[7][4] =
+{
+    1,3,5,7, // I
+    2,4,5,7, // Z
+    3,5,4,6, // S
+    3,5,4,7, // T
+    2,3,5,7, // L
+    3,5,7,6, // J
+    2,3,4,5, // O
+};
+
+/* This is responsible for the line clears
+TODO:
+Take into account scores, so double clears,
+triple clears and tetris clears
+*/
+void checkLines() {
+    int line = BOARD_HEIGHT - 1;
+    for (int i = BOARD_HEIGHT-1; i > 0; i--) {
+        int count = 0;
+        for (int j = 0; j < BOARD_WIDTH; j++) {
+            if (board[i][j]) count++;
+            board[line][j] = board[i][j];
+        }
+        if (count < BOARD_WIDTH) line--;
+    }
+}
+
 /* Returns whether or not a move is legal
 TODO:
 Have the player passed in as param (i dont like global vars :) )
@@ -32,6 +60,8 @@ bool isLegal() {
 Divide up into functions or classes
 */
 int main() {
+    srand(time(0));
+
     RenderWindow window(VideoMode(480, 960), "Ctris");
 
     Texture t1, t2;
@@ -46,9 +76,10 @@ int main() {
 
     int direction = 0;
     bool rotate = 0;
+    int level = 1;
     int colorNum = 1;
     float timer = 0;
-    float delay = 0.3;
+    float delay = 0.8 - ((level-1)*0.007);
 
     Clock clock;
     while (window.isOpen()) {
@@ -85,10 +116,51 @@ int main() {
             player[i].first += direction;
         }
 
-        // If not legal, return to prev position
         if (!isLegal())
             for (int i = 0; i < 4; i++)
                 player[i] = shadow[i];
+
+        if (rotate) {
+            std::pair<int, int> p = player[1];
+            for (int i = 0; i < 4; i++) {
+                int x = player[i].second - p.second;
+                int y = player[i].first - p.first;
+                player[i].first = p.first - x;
+                player[i].second = p.second + y;
+            }
+            if (!isLegal())
+                for (int i = 0; i < 4; i++)
+                    player[i] = shadow[i];
+        }
+
+        /* Tick */
+
+        if (timer > delay) {
+            for (int i = 0 ; i < 4; i++) {
+                shadow[i] = player[i];
+                player[i].second += 1;
+            }
+
+            if (!isLegal()) {
+                for (int i = 0; i < 4; i++)
+                    board[shadow[i].second][shadow[i].first] = colorNum;
+
+                colorNum = 1 + rand() % 7;
+                int n = rand() % 7;
+                for (int i = 0; i < 4; i++) {
+                    player[i].first = (BOARD_WIDTH/2) - (figures[n][i] % 2);
+                    player[i].second = figures[n][i] / 2;
+                }
+
+                if (!isLegal()) window.close();
+            }
+            timer = 0;
+        }
+
+        checkLines();
+        direction = 0;
+        rotate = 0;
+        delay = 0.8 - ((level-1)*0.007);
 
         /* Drawing 
 
